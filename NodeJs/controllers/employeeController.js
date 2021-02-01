e188b681aeaf4aaceeedd1ca29f0db3c299d19a3
Mahoga <mahoga26kathyani@gmail.com>
2021-02-01 15:39:42 +0530

    Backend : Create & Read Operations Done

--- NodeJs/controllers/employeeController.js
+++ NodeJs/controllers/employeeController.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const { isValidObjectId } = require('mongoose');
 var router = express.Router();
 
 var { Employee } = require('../models/employee');
 
-// => localhost : 3000/employees/list
-router.get('/list',(req,res) => {
+// => localhost : 3000/employees/Getlist
+router.get('/Getlist',(req,res) => {
     Employee.find((err, docs)=>{
         if(!err){
             res.send(docs);
@@ -16,5 +17,39 @@ router.get('/list',(req,res) => {
         }
     });
 });
+
+ router.get('/:id',(req,res)=>{
+    if(!isValidObjectId(req.body.id))
+        return res.status(400).send('No record with given id : ' + req.params.id);
+    
+    Employee.findById(req.params.id,(err,doc)=>{
+        if(!err){
+            res.send(doc);
+        }
+        else{
+            console.log('Error in retriving Employee : '+JSON.stringify(err,undefined,2))
+        }
+    })
+}) 
+
+router.post('/insertdetails',(req,res)=>{
+    var emp = new Employee({
+        name: req.body.name,
+        position : req.body.position,
+        office:req.body.office,
+        salary:req.body.salary,
+    });
+
+    emp.save((err,doc)=>{
+        if(!err){
+            res.send(doc);
+        }
+        else{
+            console.log('Error in Employee Save : ' +JSON.stringify(err,undefined,2));
+        }
+    })
+})
+
+
 
 module.exports = router;
